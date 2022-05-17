7e5b02a477d8ed4e21b48ac83ba9f3ab8431c9aa
Kouta Nakashima <naka4mak4job@gmail.com>
2022-05-17 23:01:45 +0900

    #2 Hello World をコンソールに表示する Chunk のテストを実装しました

---
+++ simplehelloworldchunk/src/test/java/com/example/simplehelloworldchunk/DataSourceConfig.java
@@ -0,0 +1,28 @@
+package com.example.simplehelloworldchunk;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+  private String url = "jdbc:mysql://localhost/springbatch_db";
+  private String username = "springbatch";
+  private String password = "springbatch";
+  private String driverClassName = "com.mysql.cj.jdbc.Driver";
+
+  @Bean
+  public DataSource createDataSource() {
+    return DataSourceBuilder
+            .create()
+            .url(url)
+            .username(username)
+            .password(password)
+            .driverClassName(driverClassName)
+            .build();
+  }
+
+}
