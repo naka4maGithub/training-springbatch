f429fd50b9f5afa51687478cb93de336708edd5d
Kouta Nakashima <naka4mak4job@gmail.com>
2022-06-25 00:36:44 +0900

    #4 Json ファイルを出力するジョブを実装しました

--- importandexportjsotocsvjobs/src/main/java/com/example/importandexportjsotocsvjobs/job/ExportTUserFromDbJobConfig.java
+++ importandexportjsotocsvjobs/src/main/java/com/example/importandexportjsotocsvjobs/job/ExportTUserFromDbJobConfig.java
@@ -1,10 +1,14 @@
 package com.example.importandexportjsotocsvjobs.job;
 
+import com.example.importandexportjsotocsvjobs.entity.TUser;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.json.JsonFileItemWriter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,10 +19,18 @@ public class ExportTUserFromDbJobConfig {
 
   private final StepBuilderFactory stepBuilderFactory;
 
+  private final JdbcPagingItemReader<TUser> tUserJdbcPagingItemReader;
+
+  private final JsonFileItemWriter<TUser> tUserJsonFileItemWriter;
+
   public ExportTUserFromDbJobConfig(JobBuilderFactory jobBuilderFactory
-          , StepBuilderFactory stepBuilderFactory) {
+          , StepBuilderFactory stepBuilderFactory
+          , @Qualifier("tUserJdbcPagingItemReader") JdbcPagingItemReader<TUser> tUserJdbcPagingItemReader
+          , @Qualifier("tUserJsonWriter") JsonFileItemWriter<TUser> tUserJsonFileItemWriter) {
     this.jobBuilderFactory = jobBuilderFactory;
     this.stepBuilderFactory = stepBuilderFactory;
+    this.tUserJdbcPagingItemReader = tUserJdbcPagingItemReader;
+    this.tUserJsonFileItemWriter = tUserJsonFileItemWriter;
   }
 
   @Bean
@@ -32,7 +44,9 @@ public class ExportTUserFromDbJobConfig {
   @Bean
   public Step exportTUserFromDbStep() {
     return stepBuilderFactory.get("exportTUserFromDbStep")
-            .tasklet(null)
+            .<TUser, TUser>chunk(100)
+            .reader(tUserJdbcPagingItemReader)
+            .writer(tUserJsonFileItemWriter)
             .build();
   }
 
