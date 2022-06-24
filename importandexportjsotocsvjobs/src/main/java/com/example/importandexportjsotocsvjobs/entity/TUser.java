a6b6e7856bb9359d1822b3b2220bb015cec209df
Kouta Nakashima <naka4mak4job@gmail.com>
2022-06-24 23:59:21 +0900

    #4 T_USER のエンティティに Json のシリアライズのアノテーションを追加しました

--- importandexportjsotocsvjobs/src/main/java/com/example/importandexportjsotocsvjobs/entity/TUser.java
+++ importandexportjsotocsvjobs/src/main/java/com/example/importandexportjsotocsvjobs/entity/TUser.java
@@ -2,7 +2,9 @@ package com.example.importandexportjsotocsvjobs.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -14,6 +16,7 @@ public class TUser {
   String familyName;
   Integer age;
   Integer workStatus;
+  @JsonSerialize(using = LocalDateTimeSerializer.class)
   @JsonDeserialize(using = LocalDateTimeDeserializer.class)
   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   LocalDateTime updateDate;
